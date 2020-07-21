089536c24a9e67ec48c06c13e0422709029ba9c5
LaLe1993 <63840101+LaLe1993@users.noreply.github.com>
2020-07-21 17:01:32 +0200

    ProfilePage update
    Now working but must be updated furder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Certifications from "./Certifications/Certifications";
 import NavBar from './Components/NavBar/NavBar';
-import SideBar from './Components/ProfilePage/SideBar/SideBar';
+import ProfilePage from './Components/ProfilePage/ProfilePage/ProfilePage';
 import Footer from './Components/Footer/Footer'
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <NavBar/>
         <Route path="/davide" exact component={() => <Certifications />} />
-        <Route path="/:id" exact component={SideBar} />
+        <Route path="/:id" exact component={ProfilePage} />
       </Router>
     </>
   );
