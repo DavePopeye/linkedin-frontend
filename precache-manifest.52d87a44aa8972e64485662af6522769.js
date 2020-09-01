4bf6be7fa343ac1c2792133f96019e9d2feef694
Ubeyt Demir <190541075@firat.edu.tr>
2020-09-01 15:43:29 +0300

    Updates

---
+++ precache-manifest.52d87a44aa8972e64485662af6522769.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b2ccec5c8c46d68681c4d1290db4bf",
+    "url": "/linkedin-frontend/index.html"
+  },
+  {
+    "revision": "4bbfeed67251550c6c0a",
+    "url": "/linkedin-frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5632adb934602a7e37dd",
+    "url": "/linkedin-frontend/static/css/main.d8c282f6.chunk.css"
+  },
+  {
+    "revision": "4bbfeed67251550c6c0a",
+    "url": "/linkedin-frontend/static/js/2.cac677d8.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/linkedin-frontend/static/js/2.cac677d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5632adb934602a7e37dd",
+    "url": "/linkedin-frontend/static/js/main.476343fc.chunk.js"
+  },
+  {
+    "revision": "f73fcdfddad7f170a58f",
+    "url": "/linkedin-frontend/static/js/runtime-main.3b535407.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/linkedin-frontend/precache-manifest.1f229e68c7d7bf4f4bf66dff7d6c311a.js"
+  "/linkedin-frontend/precache-manifest.52d87a44aa8972e64485662af6522769.js"
 );
 
 self.addEventListener('message', (event) => {
